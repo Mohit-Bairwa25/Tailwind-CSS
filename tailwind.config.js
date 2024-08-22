f752b548616cdfc02e09c80baf24001cba20a4f6
Mohit-Bairwa25 <115634456+Mohit-Bairwa25@users.noreply.github.com>
2024-08-22 20:07:56 +0530

    Custom CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './input.css',],
+    './index.html',              // Pointing to your main HTML file
+    './src/**/*.{js,ts,jsx,tsx}', // Add all JS/TS/JSX/TSX files in src
+    './input.css',                // Your custom CSS file
+  ],
   theme: {
     extend: {
-      // ...
+      spacing: {
+        13: '3.25rem',
+      },
+      fontSize: {
+        '10xl': ['10rem', { lineHeight: '1.2' }],
+      },
     },
   },
   plugins: [],
 }
-
